eb49c828a386794bb002b2b6007eeefe4dc95bb0
Takafumi Tamura <taktamur@gmail.com>
2014-01-07 21:28:43 +0900

    Initial Commit

---
+++ MoveImageCell/PAMViewController.h
@@ -0,0 +1,13 @@
+//
+//  PAMViewController.h
+//  MoveImageCell
+//
+//  Created by tak on 2014/01/07.
+//  Copyright (c) 2014年 taktamur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PAMViewController : UIViewController
+
+@end
